9a7350a21fffee883632ec107b4811a0b2f44efc
sakshamwithweb <169585315+sakshamwithweb@users.noreply.github.com>
2025-11-01 17:22:51 +0530

    updated demo

--- components/Demo.js
+++ components/Demo.js
@@ -7,7 +7,7 @@ const Demo = () => {
   return (
     <div className="py-4 border-b flex flex-col items-center">
       <h2 className='font-bold text-3xl text-center'>Demo</h2>
-      <YouTube className='mt-4' videoId="CmzKQ3PSrow" opts={{
+      <YouTube className='mt-4' videoId="-8YT85q2DlY" opts={{
         height: "270",
         width: "480"
       }} />
